00666516ef28c44532ec337141cfeff6fde864ff
watulobaraka <155726482+watulobaraka@users.noreply.github.com>
2024-01-05 16:06:32 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,6 @@
 const hamburer = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 
-// Function to open the popup after 30 seconds
-    function openPopupWithDelay() {
-        setTimeout(function () {
-            document.getElementById("overlay").style.display = "flex";
-        }, 30000); // 30 seconds in milliseconds
-    }
-
-    // Function to close the popup
-    function closePopup() {
-        document.getElementById("overlay").style.display = "none";
-    }
-
-    // Optional: You can add form validation and submission logic here.
-    document.getElementById("contactForm").addEventListener("submit", function (event) {
-        // Add your form submission logic here.
-        event.preventDefault();
-        closePopup();
-    });
-
-    // Call the function to open the popup after 30 seconds
-    openPopupWithDelay();
 
 if (hamburer) {
   hamburer.addEventListener("click", () => {
