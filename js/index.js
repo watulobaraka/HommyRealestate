cbfd84bbd5e17b19eb3539a41e42b723cc312750
watulobaraka <155726482+watulobaraka@users.noreply.github.com>
2024-01-05 15:58:28 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 const hamburer = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 
+ function openPopup() {
+        document.getElementById("overlay").style.display = "flex";
+    }
+
+    function closePopup() {
+        document.getElementById("overlay").style.display = "none";
+    }
+
+    // Optional: You can add form validation and submission logic here.
+    document.getElementById("contactForm").addEventListener("submit", function (event) {
+        // Add your form submission logic here.
+        event.preventDefault();
+        closePopup();
+    });
+
 if (hamburer) {
   hamburer.addEventListener("click", () => {
     navList.classList.toggle("open");
